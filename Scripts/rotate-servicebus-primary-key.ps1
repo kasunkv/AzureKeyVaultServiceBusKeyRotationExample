e12c8b84d1764f5376f7ef3f527bf4451e0570f6
Kasun Kodagoda <kvkrusl@gmail.com>
2018-05-07 09:38:08 +0530

    Add the powershell script

---
+++ Scripts/rotate-servicebus-primary-key.ps1
@@ -0,0 +1,25 @@
+﻿# Set the Variables
+$azureAutomationConnectionName = "AzureRunAsConnection"
+$resourceGroupName = 'KeyRotation'
+$serviceBusName = 'kvkkeyrotationbus'
+$serviceBusAccessPolicyName = 'RootManageSharedAccessKey'
+$keyVaultName = 'kvkkeyrotationvault'
+$keyVaultSecretKey = 'ServiceBusPrimaryKey'
+
+# Get the connection "AzureRunAsConnection
+$servicePrincipalConnection = Get-AutomationConnection -Name $azureAutomationConnectionName         
+
+# Adds the Authentication Account
+Add-AzureRmAccount -ServicePrincipal -TenantId $servicePrincipalConnection.TenantId -ApplicationId $servicePrincipalConnection.ApplicationId -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+
+# Regenerate the Service Bus Primary Key
+New-AzureRmServiceBusKey -ResourceGroupName $resourceGroupName -Namespace $serviceBusName -Name $serviceBusAccessPolicyName -RegenerateKey PrimaryKey
+
+# Get the newly regenerated Primary Key
+$primaryKey = (Get-AzureRmServiceBusKey -ResourceGroupName $resourceGroupName -Namespace $serviceBusName -Name $serviceBusAccessPolicyName).PrimaryKey
+
+# Convert the Primary Key to Secure String
+$secureValue = ConvertTo-SecureString $primaryKey -AsPlainText -Force
+
+# Update the Secret Value in the Key Vault
+Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name $keyVaultSecretKey -SecretValue $secureValue
